3625746fb4c735c01cd56ea663e500d607c95a86
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-09-09 21:49:37 +0200

    Build structure for stat-component

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -12,7 +12,19 @@ defineProps({
 
 <template>
   <section>
-    {{ msg }}
+    <div class="txt-content">
+      <h1>{{ hl }}</h1>
+      <p>{{ msg }}</p>
+
+      <div class="stat-counts">
+        <span>{{ companycount }}</span>
+        <span>{{ templatescount }}</span>
+        <span>{{ queriescount }}</span>
+      </div>
+    </div>
+    <div class="image-content">
+      <img src="./../assets/img/header-image.jpg" />
+    </div>
   </section>
 </template>
 
