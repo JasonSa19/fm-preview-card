457ccbab373fe4ed543fe63b0386b12c5b3d9104
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-09-09 21:35:54 +0200

    Remove not needed code and assets

---
+++ src/components/Stats.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1
+    class="text-5xl font-black text-transparent bg-gradient-to-r from-green-600 to-sky-400 bg-clip-text selection:bg-transparent"
+  >
+    {{ msg }}
+  </h1>
+</template>
+
+<style scoped></style>
